46fb7ee326be9c72ecc7c18ad185b4e434f64f72
Leonardo Engler Arruda <leonardo.earruda@gmail.com>
2022-04-07 15:47:48 -0300

    learning about lists and keys

--- src/Content.js
+++ src/Content.js
@@ -2,39 +2,34 @@ import React from "react";
 import { useState } from "react";
 
 export const Content = () => {
-  const [name, setName] = useState("Dave");
-  const [count, setCount] = useState(0);
-
-  const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave"];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-  };
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
-  const abaixaAi = () => {
-    setCount(count - 1);
-  };
-
-  const handleClick2 = (name) => {
-    console.log(count);
-  };
-
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
-  };
+  let array = [
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ];
+  const [items, setItems] = useState(array);
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {name}</p>
-      <button onClick={handleClick}>Contador: {count}</button>
-      <button onClick={abaixaAi}> - </button>
-      <button onClick={handleNameChange}>Click it.</button>
-      <button onClick={handleClick2}>Click it.</button>
-      <button onClick={(e) => handleClick3(e)}>Click it.</button>
+      <ul>
+        {items.map((item) => (
+          <li className="item">
+            <input type="checkbox" checked={item.checked}></input>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
